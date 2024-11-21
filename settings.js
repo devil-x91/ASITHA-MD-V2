2502790ddc54cd1397a255ee887118e4809ea586
LIGHT YAGAMI <164171889+devil-x91@users.noreply.github.com>
2024-11-21 14:42:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=CuYzSZBb#odCakyKIHCrjoj4ZdjsBuGh8fhZgxWOq1BLPcmJ9kW0' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Cqw31KRS#23so8rZbEVAjp4l8Sd8VyqcorycLkP2xQJ2ClSRIgN8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
